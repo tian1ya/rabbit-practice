d0945dd63595ead6d6662708d955800d00749a5f
“lx” <lx@qq.com>
2023-08-08 11:12:47 +0800

    add element-plus and its module used in dev

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import './assets/main.css'
 
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+// import { createRouter } from 'vue-router'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+// app.use(router)
+app.mount('#app')
