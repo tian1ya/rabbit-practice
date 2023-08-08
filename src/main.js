316d408aac67edffccd5b03070e7841a88961d95
“lx” <lx@qq.com>
2023-08-08 12:35:40 +0800

    install axios and test apis

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import './assets/main.css'
 
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
+import {
+  createPinia
+} from 'pinia'
+
+import {
+  createApp
+} from 'vue'
 // import { createRouter } from 'vue-router'
 
 import App from './App.vue'
+
+import {
+  getCategoryAPI
+} from '@/apis/testAPI'
+
+getCategoryAPI().then(res => {
+  console.log(res);
+})
 
 const app = createApp(App)
 app.use(createPinia())
