047a41aa2e3eaf32e8f046bb0281122ae05aa424
“lx” <lx@qq.com>
2023-08-08 12:25:33 +0800

    install scss and reset element theme

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
 
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <el-button type="primary"> button </el-button>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+// 导入对应包
+import ElementPlus from 'unplugin-element-plus/vite'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -15,14 +18,30 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      // 配置采用sass 样式配置系统
+      resolvers: [ElementPlusResolver({importStyle: "sass"})],
     }),
+    // 按需定制主题配置
+    ElementPlus({
+      useSource: true,
+    }),
+
     vue(),
   ],
   // 实际的路径转换，@ -> src
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
