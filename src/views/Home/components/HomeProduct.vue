96c6328f709023508926425b27d86ece8a26bf6a
“lx” <lx@qq.com>
2023-08-09 18:29:31 +0800

    abstract goodItem component to reduce repeat

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HomePanel from "./HomePanel.vue";
 import { getGoodsAPI } from "@/apis/home";
+import GoodsItems from './GoodItem.vue'
 import { ref } from "vue";
 
 const goodsProduct = ref([]);
@@ -27,12 +28,7 @@ getGoods()
           </RouterLink>
           <ul class="goods-list">
             <li v-for="good in cate.goods" :key="good.id">
-              <RouterLink to="/" class="goods-item">
-                <img v-img-lazy="good.picture" alt="" />
-                <p class="name ellipsis">{{ good.name }}</p>
-                <p class="desc ellipsis">{{ good.desc }}</p>
-                <p class="price">&yen;{{ good.price }}</p>
-              </RouterLink>
+              <GoodsItems :goods="good"></GoodsItems>
             </li>
           </ul>
         </div>
@@ -128,41 +124,7 @@ getGoods()
       }
     }
 
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all 0.5s;
-
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
-      }
-    }
+    
   }
 }
 </style>
