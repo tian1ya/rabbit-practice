3ad387c89b7f64c44b1a8eadeb6aeb395f0f1535
“lx” <lx@qq.com>
2023-08-09 15:22:02 +0800

    home banner done

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,6 +1,44 @@
 <script setup>
+import { getBannerAPI } from "@/apis/home";
+import { onMounted, ref } from "vue";
+
+const banners = ref([]);
+const getBanners = async () => {
+  const res = await getBannerAPI();
+  banners.value = res.result;
+  console.log(res);
+};
+
+onMounted(() => getBanners());
 </script>
 
 <template>
-  <div> HomeBanner </div>
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in banners" :key="item.id">
+        <img
+          :src="item.imgUrl"
+          alt=""
+        />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+
+
+<style scoped lang='scss'>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
