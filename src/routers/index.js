1a06e24cbcde27d08379b41f62234011eda820f9
“lx” <lx@qq.com>
2023-08-15 19:50:34 +0800

    user order and info init and third router

--- src/routers/index.js
+++ src/routers/index.js
@@ -11,9 +11,12 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
-import cartList from '@/views/cartList/index.vue'
+import cartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(
@@ -40,6 +43,16 @@ const router = createRouter({
     },{
       path: 'checkout',
       component: Checkout
+    }, {
+      path: 'member',
+      component: Member,
+      children: [{
+        path: 'order',
+        component: UserOrder,
+      },{
+        path: 'user',
+        component: UserInfo,
+      }]
     }]
   }, {
     path: '/login',
